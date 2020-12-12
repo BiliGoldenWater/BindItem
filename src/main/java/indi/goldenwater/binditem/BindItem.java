0a1431d1d08b2cb4b47b63ea67cf112fc1a67fc7
BiliGoldenWater <2439577268@qq.com>
2020-12-12 20:38:47 +0800

    add init database function

--- src/main/java/indi/goldenwater/binditem/BindItem.java
+++ src/main/java/indi/goldenwater/binditem/BindItem.java
@@ -5,6 +5,11 @@ import indi.goldenwater.binditem.enchant.RegisterEnchantBindItem;
 import indi.goldenwater.binditem.module.DBOperator;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public final class BindItem extends JavaPlugin {
     private static BindItem instance;
     private static boolean useUUID;
@@ -22,6 +27,8 @@ public final class BindItem extends JavaPlugin {
 
 //        getServer().getPluginManager().registerEvents(,this);
 
+        getLogger().info("init database.");
+        initEnchantLvlDatabase(enchantLvlDatabase);
         RegisterEnchantBindItem.registerEnchantBindItem();
         BindItemExecutor.registerCommandBindItem(this);
         getLogger().info("BindItem Enabled");
@@ -32,6 +39,43 @@ public final class BindItem extends JavaPlugin {
         // Plugin shutdown logic
         saveConfig();
         getLogger().info("BindItem Disabled");
+    }
+
+    private void initEnchantLvlDatabase(DBOperator enchantLvlDatabase){
+        Map<String, String> tableStyle = new HashMap<>();
+        tableStyle.put("enchantLvl", "int");
+        tableStyle.put("playerName", "text");
+        tableStyle.put("bindNum", "int");
+        enchantLvlDatabase.createTable("enchantData", tableStyle);
+
+        List<String> columns = new ArrayList<>(), values = new ArrayList<>();
+        columns.add("enchantLvl");
+        columns.add("playerName");
+        columns.add("bindNum");
+
+        Map<Integer, String> preSettings = new HashMap<>();
+        preSettings.put(0, "\"bindonpickup\"");
+        preSettings.put(1, "\"bindonuse\"");
+        preSettings.put(2, "\"bindonequip\"");
+        preSettings.put(3, "\"none\"");
+        for(int i = 0;i<=9;i++){
+            values.clear();
+            values.add(String.valueOf(i));
+            values.add(preSettings.get(Math.min(i, 3)));
+            values.add("0");
+            if(enchantLvlDatabase.select("enchantData", "enchantLvl", "enchantLvl=" + i, 1).get(1).size()>0){
+                enchantLvlDatabase.update("enchantData", columns, values, "enchantLvl=" + i);
+            } else {
+                enchantLvlDatabase.insert("enchantData", columns, values);
+            }
+        }
+
+        List<Object> noBindEnchant = enchantLvlDatabase.select("enchantData", "enchantLvl", "bindNum<=0", 1).get(1);
+        noBindEnchant.forEach((value)->{
+            if((int)value>9){
+                enchantLvlDatabase.delete("enchantData", "enchantLvl="+value);
+            }
+        });
     }
 
     public static BindItem getInstance(){
